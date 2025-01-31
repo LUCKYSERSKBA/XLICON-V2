a77049b2988733d46bb55deef8250543b8ea89ce
Lucky <140100739+LUCKYSERSKBA@users.noreply.github.com>
2025-01-31 05:48:03 +0530

    𝐂𝐫𝐞𝐚𝐭𝐞 𝐛𝐲 𝐝𝐚𝐫𝐤 𝐩𝐫𝐢𝐧𝐜𝐞

--- plugins/channel.js
+++ plugins/channel.js
@@ -2,9 +2,9 @@ let handler = async m => m.reply(`
 
 ╭⭑⭑⭑★✪ *XLICON* ✪★⭑⭑⭑
 │ 📂 *BOT Name:* _XLICON-V2-MD_
-│ 📝 *Description:* _I'm XLICON-V2. A MultiDevice WhatsApp bot with rich features Created By SALMAN AMAD and Abraham Dwamena._
-│ 👤 *Owner:* _Salman Ahmad_
-│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e
+│ 📝 *Description:* _I'm XLICON-V2. A MultiDevice WhatsApp bot with rich features Created By Aadhiser and LUCKYSERSKBA._
+│ 👤 *Owner:* _Aadhiser_
+│ 🌐 *Channel:* https://whatsapp.com/channel/0029VayY2vu1NCrLQHzmRB2Q
 ╰━━━━━━━━━━━━━━━━╯
 `.trim())
 handler.help = ['channel']
